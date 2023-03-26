a9996284440a8fc61ceaadfaaf6f16832761f29f
lua1235 <austinlu1235@gmail.com>
2023-03-26 15:25:34 -0600

    commit

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ function grade() {
                     document.getElementById("qTwo"),
                     document.getElementById("qThree"),
                     document.getElementById("qFour"),
-                    document.getElementById("qFive"),]
-    const answers = ["body", "img", "alt", "input", "text"];
+                    document.getElementById("qFive"),
+                    document.getElementById("qSix")]
+    const answers = ["body", "img", "alt", "input", "text", "script"];
     let score = 0;
     for(let i in answers) {
         if(questions[i].value.toLowerCase() == answers[i]) {
             score ++;
         }
     }
-    if(score == 5) {
-        alert("You scored "+score+"/5 \nCongratulations!")
+    if(score == 6) {
+        alert("You scored "+score+"/6 \nCongratulations!")
     }else{
-        alert("You scored "+score+"/5")
+        alert("You scored "+score+"/6")
     }
 }
